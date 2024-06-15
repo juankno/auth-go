2c283be81c46c9acb45de5559814810feb32f5db
Juankno <juanes0202@gmail.com>
2024-06-15 13:16:38 -0500

    feat: create a repository file with the methods that modify the database

---
+++ repository/user_repository.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"go-auth/models"
+
+	"github.com/jinzhu/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(user *models.User) error
+	FindByEmail(email string) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
+}
+
+func (r *userRepository) CreateUser(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}

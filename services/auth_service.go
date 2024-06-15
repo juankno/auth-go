2b5687475dd5e922014cba81ba62ec8c8260e74f
Juankno <juanes0202@gmail.com>
2024-06-15 13:24:30 -0500

    feat: create a service with the logic to create a new user and login

---
+++ services/auth_service.go
@@ -0,0 +1,54 @@
+package services
+
+import (
+	"go-auth/models"
+	"go-auth/repository"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	Register(user *models.User) error
+	Login(email, password string) (string, error)
+}
+
+type authService struct {
+	userRepository repository.UserRepository
+}
+
+func NewAuthService(userRepository repository.UserRepository) AuthService {
+	return &authService{userRepository}
+}
+
+func (s *authService) Register(user *models.User) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return s.userRepository.CreateUser(user)
+}
+
+func (s *authService) Login(email, password string) (string, error) {
+	user, err := s.userRepository.FindByEmail(email)
+	if err != nil {
+		return "", err
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}

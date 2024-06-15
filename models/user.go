afdb3e47f13051ed59fb3663a742fd6fdcea400c
Juankno <juanes0202@gmail.com>
2024-06-15 15:20:43 -0500

    refactor: change columns name

--- models/user.go
+++ models/user.go
@@ -1,12 +1,15 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type User struct {
-	gorm.Model
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6"`
+	ID        uint       `gorm:"column:id;primaryKey"`
+	Name      string     `json:"name" validate:"required"`
+	Email     string     `json:"email" validate:"required,email" gorm:"unique"`
+	Password  string     `json:"password" validate:"required,min=6"`
+	CreatedAt time.Time  `gorm:"column:created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at"`
+	DeletedAt *time.Time `gorm:"column:deleted_at;index"`
 }

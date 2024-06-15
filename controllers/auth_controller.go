e395dd53983d2908534b39754090f74496c6ee0b
Juankno <juanes0202@gmail.com>
2024-06-15 13:18:30 -0500

    feat: create a controller file with the endpoint to register and login user

---
+++ controllers/auth_controller.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"go-auth/models"
+	"go-auth/services"
+	"go-auth/utils"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type AuthController struct {
+	authService services.AuthService
+}
+
+func NewAuthController(authService services.AuthService) *AuthController {
+	return &AuthController{authService}
+}
+
+func (c *AuthController) Register(ctx echo.Context) error {
+	user := new(models.User)
+	if err := ctx.Bind(user); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid input"})
+	}
+
+	if err := utils.ValidateStruct(user); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	if err := c.authService.Register(user); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to register user"})
+	}
+
+	return ctx.JSON(http.StatusCreated, user)
+}
+
+func (c *AuthController) Login(ctx echo.Context) error {
+	var login struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required"`
+	}
+
+	if err := ctx.Bind(&login); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid input"})
+	}
+
+	if err := utils.ValidateStruct(&login); err != nil {
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	token, err := c.authService.Login(login.Email, login.Password)
+	if err != nil {
+		return ctx.JSON(http.StatusUnauthorized, map[string]string{"error": "Invalid email or password"})
+	}
+
+	return ctx.JSON(http.StatusOK, map[string]string{"token": token})
+}

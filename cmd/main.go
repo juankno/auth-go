3aa72b53312f24c1a20a57fa623421d1b3c2b466
Juankno <juanes0202@gmail.com>
2024-06-16 11:47:57 -0500

    refactor: use file to custom routes

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"go-auth/config"
-	"go-auth/controllers"
-	"go-auth/middleware"
-	"go-auth/repository"
-	"go-auth/services"
+	"go-auth/routes"
 	"go-auth/utils"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,20 +14,8 @@ func main() {
 	db := config.InitDB()
 	defer db.Close()
 
-	userRepository := repository.NewUserRepository(db)
-	authService := services.NewAuthService(userRepository)
-	authController := controllers.NewAuthController(authService)
-
-	e.POST("/register", authController.Register)
-	e.POST("/login", authController.Login)
-
-	r := e.Group("/restricted")
-	r.Use(middleware.JWTMiddleware())
-	r.GET("/user", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{"message": "You are authorized"})
-	})
+	routes.InitRoutes(e, db)
 
 	port := utils.GetPort()
-
 	e.Logger.Fatal(e.Start(port))
 }
